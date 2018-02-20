9f68bbaf102254fc7060d1f2baafbf6480fd6fc5
Prachi Patil <patilprachi16jul@gmail.com>
2018-02-20 12:00:44 -0500

    FCN-232 Changes in search user functionality.

--- restapi/src/main/java/org/fnovella/project/user/model/UserSearch.java
+++ restapi/src/main/java/org/fnovella/project/user/model/UserSearch.java
@@ -1,68 +1,93 @@
 package org.fnovella.project.user.model;
 
+import java.util.List;
+
 import org.fnovella.project.user.repository.UserRepository;
 import org.fnovella.project.utility.APIUtility;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public class UserSearch {
 
-	private String documentValue;
-	private String firstName;
-	private String appCode;
+    private String documentValue;
+    private String firstName;
+    private String appCode;
+    private String secondName;
+    private String firstLastName;
+    private String secondLastName;
 
-	public String getDocumentValue() {
-		return documentValue;
-	}
+    public String getDocumentValue() {
+        return documentValue;
+    }
 
-	public void setDocumentValue(String documentValue) {
-		this.documentValue = documentValue;
-	}
+    public void setDocumentValue(String documentValue) {
+        this.documentValue = documentValue;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getAppCode() {
-		return appCode;
-	}
-	public void setAppCode(String appCode) {
-		this.appCode = appCode;
-	}
-	
-	public Page<AppUser> getResults(UserRepository userRepository, Pageable pageable) {
+    public String getSecondName() {
+        return secondName;
+    }
 
-		boolean useDocument = this.documentValue != null && !this.documentValue.trim().equals("");
-		boolean useName = APIUtility.isNotNullOrEmpty(this.firstName);
-		boolean useAppCode = APIUtility.isNotNullOrEmpty(this.appCode);
-		if (useName && useAppCode && useDocument) {
-			List<AppUser> users = userRepository.findByFirstNameStartingWithAndAppCodeAndDocumentValue(this.firstName, this.appCode, this.documentValue);
-			return new PageImpl<>(users, pageable, users.size());
-		} else if (useName && useAppCode) {
-			List<AppUser> users = userRepository.findByFirstNameStartingWithAndAppCode(this.firstName, this.appCode);
-			return new PageImpl<>(users, pageable, users.size());
-		} else if (useName && useDocument) {
-			List<AppUser> users = userRepository.findByFirstNameStartingWithAndDocumentValue(this.firstName, this.documentValue);
-			return new PageImpl<>(users, pageable, users.size());
-		}  else if (useAppCode && useDocument) {
-			return userRepository.findByAppCodeAndDocumentValue(this.appCode, this.documentValue, pageable);
-		}  else if (useName) {
-			List<AppUser> users = userRepository.findByFirstNameStartingWith(this.firstName);
-			return new PageImpl<>(users, pageable, users.size());
-		}  else if (useAppCode) {
-			return userRepository.findByAppCode(this.appCode, pageable);
-		}  else if (useDocument) {
-			return userRepository.findByDocumentValue(this.documentValue, pageable);
-		} else {
-			return userRepository.findAll(pageable);
-		}
-	}
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
 
+    public String getAppCode() {
+        return appCode;
+    }
 
+    public void setAppCode(String appCode) {
+        this.appCode = appCode;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstLastName() {
+        return firstLastName;
+    }
+
+    public void setFirstLastName(String firstLastName) {
+        this.firstLastName = firstLastName;
+    }
+
+    public String getSecondLastName() {
+        return secondLastName;
+    }
+
+    public void setSecondLastName(String secondLastName) {
+        this.secondLastName = secondLastName;
+    }
+
+    public Page<AppUser> getResults(final UserRepository userRepository, final Pageable pageable) {
+        final String firstName = APIUtility.isNotNullOrEmpty(this.firstName) ? this.firstName : "";
+        final String secondName = APIUtility.isNotNullOrEmpty(this.secondName) ? this.secondName : "";
+        final String firstLastName = APIUtility.isNotNullOrEmpty(this.firstLastName) ? this.firstLastName : "";
+        final String secondLastName = APIUtility.isNotNullOrEmpty(this.secondLastName) ? this.secondLastName : "";
+        final boolean useDocument = APIUtility.isNotNullOrEmpty(this.documentValue);
+        final boolean useAppCode = APIUtility.isNotNullOrEmpty(this.appCode);
+        if (useAppCode && useDocument) {
+            final List<AppUser> users = userRepository.findByNamesStartingWithAndAppCodeAndDocumentValue(
+                    firstName, secondName, firstLastName, secondLastName, this.appCode, this.documentValue);
+            return new PageImpl<>(users, pageable, users.size());
+        } else if (useAppCode) {
+            final List<AppUser> users = userRepository.findByNamesStartingWithAndAppCode(
+                    firstName, secondName, firstLastName, secondLastName, this.appCode);
+            return new PageImpl<>(users, pageable, users.size());
+        } else if (useDocument) {
+            final List<AppUser> users = userRepository.findByNamesStartingWithAndDocumentValue(
+                    firstName, secondName, firstLastName, secondLastName, this.documentValue);
+            return new PageImpl<>(users, pageable, users.size());
+        } else {
+            final List<AppUser> users = userRepository.findByNamesStartingWith(
+                    firstName, secondName, firstLastName, secondLastName);
+            return new PageImpl<>(users, pageable, users.size());
+        }
+    }
 }
--- restapi/src/main/java/org/fnovella/project/user/repository/UserRepository.java
+++ restapi/src/main/java/org/fnovella/project/user/repository/UserRepository.java
@@ -5,8 +5,6 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.fnovella.project.user.model.AppUser;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -22,15 +20,32 @@ public interface UserRepository extends JpaRepository<AppUser, Integer> {
 	AppUser findByEmailAndPassword(String email, String password);
 	AppUser findByEmail(String email);
 	AppUser findByDocumentValue(String documentValue);
-	@Query(value = "SELECT * FROM APP_USER where first_name like ?1% COLLATE Latin1_General_CI_AI", nativeQuery = true)
-	List<AppUser> findByFirstNameStartingWith(String firstName);
-	Page<AppUser> findByAppCode(String appCode, Pageable pageable);
-	Page<AppUser> findByDocumentValue(String documentValue, Pageable pageable);
-	@Query(value = "SELECT * FROM APP_USER where first_name like ?1% COLLATE Latin1_General_CI_AI AND document_value = ?2", nativeQuery = true)
-	List<AppUser> findByFirstNameStartingWithAndDocumentValue(String firstName, String documentValue);
-	@Query(value = "SELECT * FROM APP_USER where first_name like ?1% COLLATE Latin1_General_CI_AI AND app_code = ?2", nativeQuery = true)
-	List<AppUser> findByFirstNameStartingWithAndAppCode(String firstName, String appCode);
-	@Query(value = "SELECT * FROM APP_USER where first_name like ?1% COLLATE Latin1_General_CI_AI AND app_code = ?2 AND document_value = ?3", nativeQuery = true)
-	List<AppUser> findByFirstNameStartingWithAndAppCodeAndDocumentValue(String firstName, String appCode, String documentValue);
-	Page<AppUser> findByAppCodeAndDocumentValue(String appCode, String documentValue, Pageable pageable);
+	@Query(value = "SELECT * FROM APP_USER WHERE first_name LIKE ?1% COLLATE Latin1_General_CI_AI "
+	        + "AND second_name LIKE ?2% COLLATE Latin1_General_CI_AI "
+	        + "AND first_lastname LIKE ?3% COLLATE Latin1_General_CI_AI "
+	        + "AND second_lastname LIKE ?4% COLLATE Latin1_General_CI_AI ", nativeQuery = true)
+	List<AppUser> findByNamesStartingWith(
+	        String firstName, String secondName, String firstLastName, String secondLastName);
+	@Query(value = "SELECT * FROM APP_USER WHERE first_name LIKE ?1% COLLATE Latin1_General_CI_AI "
+	        + "AND second_name LIKE ?2% COLLATE Latin1_General_CI_AI "
+            + "AND first_lastname LIKE ?3% COLLATE Latin1_General_CI_AI "
+            + "AND second_lastname LIKE ?4% COLLATE Latin1_General_CI_AI "
+	        + "AND document_value = ?5", nativeQuery = true)
+	List<AppUser> findByNamesStartingWithAndDocumentValue(
+	        String firstName, String secondName, String firstLastName, String secondLastName, String documentValue);
+	@Query(value = "SELECT * FROM APP_USER WHERE first_name LIKE ?1% COLLATE Latin1_General_CI_AI "
+	        + "AND second_name LIKE ?2% COLLATE Latin1_General_CI_AI "
+            + "AND first_lastname LIKE ?3% COLLATE Latin1_General_CI_AI "
+            + "AND second_lastname LIKE ?4% COLLATE Latin1_General_CI_AI "
+	        + "AND app_code = ?5", nativeQuery = true)
+	List<AppUser> findByNamesStartingWithAndAppCode(
+	        String firstName, String secondName, String firstLastName, String secondLastName, String appCode);
+	@Query(value = "SELECT * FROM APP_USER WHERE first_name LIKE ?1% COLLATE Latin1_General_CI_AI "
+	        + "AND second_name LIKE ?2% COLLATE Latin1_General_CI_AI "
+            + "AND first_lastname LIKE ?3% COLLATE Latin1_General_CI_AI "
+            + "AND second_lastname LIKE ?4% COLLATE Latin1_General_CI_AI "
+	        + "AND app_code = ?5 "
+	        + "AND document_value = ?6", nativeQuery = true)
+	List<AppUser> findByNamesStartingWithAndAppCodeAndDocumentValue(
+	        String firstName, String secondName, String firstLastName, String secondLastName, String appCode, String documentValue);
 }
